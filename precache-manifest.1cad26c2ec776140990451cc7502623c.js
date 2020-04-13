8a2f026189a51b097295d1258f404ffecfb08afd
ecyrbe <ecyrbe@gmail.com>
2020-04-13 17:44:09 +0200

    Updates

---
+++ precache-manifest.1cad26c2ec776140990451cc7502623c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faa9c8b7ec4fa3fc900c90323e516507",
+    "url": "/triste/index.html"
+  },
+  {
+    "revision": "8f6ff79a4f16c8440a98",
+    "url": "/triste/static/css/main.cee5d9f6.chunk.css"
+  },
+  {
+    "revision": "254e72895677f6878d9f",
+    "url": "/triste/static/js/2.e0edb373.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/triste/static/js/2.e0edb373.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f6ff79a4f16c8440a98",
+    "url": "/triste/static/js/main.7ef0e038.chunk.js"
+  },
+  {
+    "revision": "0f2195081a84a061ea81",
+    "url": "/triste/static/js/runtime-main.cf883b99.js"
+  }
+]);
